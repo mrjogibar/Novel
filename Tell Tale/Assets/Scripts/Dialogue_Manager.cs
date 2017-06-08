eba2bbd2e4aed3a15a96b3cda5e830b6bf515593
Sven Berzellis <bt702653@uni-bayreuth.de>
2017-06-08 20:06:29 +0200

    Arbeit an Narrationsystem

---
+++ Tell Tale/Assets/Scripts/Dialogue_Manager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Dialogue_Manager : MonoBehaviour {
+
+	//Singleton property
+	public static Dialogue_Manager Instance { get; private set; }
+
+	void Awake()
+	{
+		if (Instance != null && Instance != this) 
+		{
+			Destroy(gameObject);
+		}
+
+		Instance = this;
+
+		gameObject.AddComponent<AudioSource> ();
+	}
+
+	public void BeginDialogue (AudioClip passedClip)
+	{
+        //set and play the audio-clip
+        audio = passedClip;
+        audio.PlayOneShot ();
+	}
+}
---
+++ Tell Tale/Assets/Scripts/Narrator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Narrator : MonoBehaviour {
+
+	public AudioClip dialogueClip;
+
+	void Update ()
+	{
+		if (Input.GetKeyDown (KeyCode.Space)) 
+		{
+			Dialogue_Manager.Instance.BeginDialogue(dialogueClip);
+		}
+	}
+}
